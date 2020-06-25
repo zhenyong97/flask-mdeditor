02949e4456b97dc8738909624d4a49a989511931
callmehero <506141737@qq.com>
2020-06-25 16:24:12 +0800

    set pypifile

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
 # -*- coding:utf-8 -*-
 import os
-from setuptools import find_packages, setup
-from codecs import open
+from setuptools import setup, find_packages
 
 with open(os.path.join(os.path.dirname(__file__), 'README.md'), encoding='utf-8') as readme:
     long_description = readme.read()
@@ -13,14 +12,14 @@ setup(
     name='Flask-MDEditor',
     version='0.1',
     packages=['flask_mdeditor'],
-    include_package_data=True,
-    license='MTI License',
-    description='A simple FLASK app to edit markdown text.',
+    license='MIT',
+    description='MDEditor integration for Flask',
     long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/callmehero/flask-mdeditor',
-    author='ZiscLi',
+    author='Ziscli',
     author_email='lzyong2019@gmail.com',
+    include_package_data=True,
     install_requires=[
         'Flask'
     ],
@@ -31,7 +30,6 @@ setup(
         'Operating System :: OS Independent',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python',
-        # Replace these appropriately if you are stuck on Python 2.
         'Programming Language :: Python :: 3.7',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
